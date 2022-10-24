29154fe2afe28461019a5f8942b3192380e715ca
Tran Huynh <56352798+jarm237@users.noreply.github.com>
2022-10-25 01:12:22 +0700

    Add files via upload

--- read_img.py
+++ read_img.py
@@ -1,36 +1,35 @@
 import os
-# import pytesseract
-# pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-Path = "Image"
-myImage = os.listdir(Path)
-core = "password_record.png"
-if core in myImage:
-    image = cv2.imread('Image/password_record.png', cv2.IMREAD_UNCHANGED)
-else:
-    image = cv2.imread('empty_pw/blank.png', cv2.IMREAD_UNCHANGED)
-
-cs_config = r'-c tessedit_char_whitelist=ABCDEFGH123456789 --psm 7 --oem 3'
+# Path = "Image"
+# myImage = os.listdir(Path)
+# core = "password_record.png"
+# if core in myImage:
+#     image = cv2.imread('Image/password_record.png', cv2.IMREAD_UNCHANGED)
+# else:
+#     image = cv2.imread('empty_pw/blank.png', cv2.IMREAD_UNCHANGED)
+#
+# cs_config = r'-c tessedit_char_whitelist=ABCDEFGH123456789 --psm 7 --oem 3'
 # image_text = pytesseract.image_to_string(image, lang='eng', config=cs_config)
 
 # reading image
 img = cv2.imread('Image/password_record.png')
 
 # converting image into grayscale image
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+# imgBlur = cv2.GaussianBlur(imgGray, (7, 7), 1)
+# imgCanny = cv2.Canny(imgBlur, 50, 50)
 
 # setting threshold of gray image
-_, threshold = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY)
+_, threshold = cv2.threshold(imgGray, 127, 255, cv2.THRESH_BINARY)
 
 # using a findContours() function
 contours, _ = cv2.findContours(
     threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
 i = 0
-
 # list for storing names of shapes
 for contour in contours:
 
@@ -57,14 +56,22 @@ for contour in contours:
         y = int(M['m01'] / M['m00'])
 
     # putting shape name at center of each shape
-    if len(approx) == 3:
+    if len(approx) == 1:
+        image_text = 'Line'
+
+    elif len(approx) == 3:
         # cv2.putText(img, 'Triangle', (x, y),
         #             cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 2)
         image_text = 'Triangle'
 
+    # if len(approx) == 3:
+    #     # cv2.putText(img, 'Triangle', (x, y),
+    #     #             cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 2)
+    #     image_text = 'Triangle'
+
     elif len(approx) == 4:
         # cv2.putText(img, 'Quadrilateral', (x, y),
-        #             cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 2)
+        #             cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 2)
         # image_text = 'Quadrilateral'
         aspRatio = w / float(h)
         if aspRatio > 0.95 and aspRatio < 1.05:
@@ -87,6 +94,11 @@ for contour in contours:
         #             cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 2)
         image_text = 'circle'
 
+
+
+cv2.imwrite("Image/imgCanvas.png", img)
+# cv2.imshow("test", img)
+# cv2.waitKey(0)
 # displaying the image after drawing contours
 # cv2.imshow('shapes', img)
 #
